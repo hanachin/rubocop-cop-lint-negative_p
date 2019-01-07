c0f668b0e8c89459d812517c38c8013b86c73d8d
Seiei Miyagi <hanachin@gmail.com>
2019-01-08 00:52:50 +0900

    Init rspec

---
+++ spec/rubocop/cop/lint/positive_p_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Rubocop::Cop::Lint::PositiveP do
+  it "has a version number" do
+    expect(Rubocop::Cop::Lint::PositiveP::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
